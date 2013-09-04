b9dd7e6804f563f46c00d9aafa0ae84cceb2cbc3
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-09-04 18:03:38 +0200

    Refactor and clean up.

--- src/dtk/dispatch.d
+++ src/dtk/dispatch.d
@@ -54,15 +54,12 @@ static:
         _dtkCallbackInitialized = true;
     }
 
-    // all the event arguments captures by the bind command (todo: %W should be caught)
-    // enum string eventArgs = "%W %w %x %y %k %K %h %X %Y";
-
     // todo: add all bindings here
     private static void _initDtkInterceptor()
     {
         /** Hook keyboard" */
 
-        // Note: We're retrieving keysym, not keycode
+        // Note: We're retrieving a keysym, not a keycode
         static immutable keyboardArgs =
             [TkSubs.keysym_decimal, TkSubs.uni_char, TkSubs.state, TkSubs.widget_path, TkSubs.rel_x_pos, TkSubs.rel_y_pos, TkSubs.abs_x_pos, TkSubs.abs_y_pos, TkSubs.timestamp].join(" ");
 
@@ -71,33 +68,15 @@ static:
 
         /** Hook mouse. */
 
-        /*
-            Note: We should try not to use Tk's double, triple, etc, modifiers for tracking multiple
-            clicks because they're unreliable. E.g. a double-left-click followed by a single right
-            click will generate two double-click events for this binding:
-
-            bind . <Double-ButtonPress> { }
-
-            THe current workaround is to explicitly map each mouse combination, e.g.:
-
-            bind . <Double-ButtonPress-1> { }
-            bind . <Double-ButtonPress-3> { }
-
-            But this won't let us provide configuration for any specific mouse delay a user
-            might want, e.g. for use with accessibility features or widgets with a specific
-            input behavior.
-        */
-
         static immutable mouseArgs = [cast(string)TkSubs.mouse_wheel_delta, TkSubs.state, TkSubs.widget_path, TkSubs.rel_x_pos, TkSubs.rel_y_pos, TkSubs.abs_x_pos, TkSubs.abs_y_pos, TkSubs.timestamp].join(" ");
 
         static mouseButtons = [MouseButton.button1, MouseButton.button2, MouseButton.button3, MouseButton.button4, MouseButton.button5];
 
-        static modifiers = ["", "Double-", "Triple-", "Quadruple-"];
-
-        static mouseActions = [MouseAction.click, MouseAction.double_click, MouseAction.triple_click, MouseAction.quadruple_click];
-
         foreach (idx, mouseButton; mouseButtons)
         {
+            static immutable modifiers = ["", "Double-", "Triple-", "Quadruple-"];
+            static immutable mouseActions = [MouseAction.click, MouseAction.double_click, MouseAction.triple_click, MouseAction.quadruple_click];
+
             foreach (modifier, mouseAction; zip(modifiers, mouseActions))
             {
                 // note: button click can be double clicks, triple clicks, etc.
@@ -121,14 +100,44 @@ static:
                     _dtkCallbackIdent, EventType.mouse, MouseAction.wheel, cast(string)TkSubs.mouse_button, mouseArgs);
     }
 
-    //~ static auto safeToInt(T)(T* val)
-    //~ {
-        //~ auto res = to!string(val);
-        //~ if (res == "??")  // note: edge-case, but there might be more of them
-            //~ return 0;    // note2: "0" is just a guess, not sure what else to set it to.
+    static extern(C)
+    int dtkCallbackHandler(ClientData clientData, Tcl_Interp* interp, int objc, const Tcl_Obj** argArr)
+    {
+        if (objc < 2)  // DTK event signals need at least 2 arguments
+            return TCL_OK;
 
-        //~ return to!int(res);
-    //~ }
+        /**
+            Indices:
+                0  => Name of this callback.
+                1  => The EventType.
+                2+ => The data, based on the Tcl substitutions that we provided for the EventType.
+                      EventType-specific handlers take these arguments.
+        */
+
+        version (DTK_LOG_EVENTS)
+        {
+            import std.stdio;
+            foreach (idx; 0 .. objc)
+            {
+                stderr.writefln("-- received #%s: %s", idx + 1, argArr[idx].tclPeekString());
+            }
+        }
+
+        EventType type = to!EventType(argArr[1].tclPeekString());
+        auto args = argArr[2 .. objc];
+
+        switch (type) with (EventType)
+        {
+            case mouse:    return _handleMouseEvent(args);
+            case keyboard: return _handleKeyboardEvent(args);
+            case button:   return _handleButtonEvent(args);
+            default:       assert(0, format("Unhandled event type '%s'.", type));
+        }
+    }
+
+    // no-op
+    static extern(C)
+    void dtkCallbackDeleter(ClientData clientData) { }
 
     private enum TkEventFlag : uint
     {
@@ -157,17 +166,17 @@ static:
                 9 => Timestamp
         */
 
-        MouseAction action = to!MouseAction(tclArr[0].tclPeekString());
+        MouseAction action = getTclMouseAction(tclArr[0]);
         MouseButton button = getTclMouseButton(tclArr[1]);
         int wheelDelta = getTclWheelDelta(tclArr[2]);
         KeyMod keyMod = getTclKeyMod(tclArr[3]);
 
-        Widget widget = Widget.lookupWidgetPath(tclArr[4].tclPeekString());
+        Widget widget = getTclWidget(tclArr[4]);
         assert(widget !is null);
 
         Point widgetMousePos = getTclPoint(tclArr[5 .. 7]);
         Point desktopMousePos = getTclPoint(tclArr[7 .. 9]);
-        TimeMsec timeMsec = to!TimeMsec(tclArr[9].tclPeekString());
+        TimeMsec timeMsec = getTclTimestamp(tclArr[9]);
 
         auto event = scoped!MouseEvent(widget, action, button, wheelDelta, keyMod, widgetMousePos, desktopMousePos, timeMsec);
         return _dispatchEvent(widget, event);
@@ -191,27 +200,16 @@ static:
                 8  => Timestamp
         */
 
-        // note: change this to EnumBaseType after Issue 10942 is fixed for KeySym.
-        alias keyBaseType = long;
+        KeySym keySym = getTclKeySym(tclArr[0]);
+        char uniChar = getTclUniChar(tclArr[1]);
+        KeyMod keyMod = getTclKeyMod(tclArr[2]);
 
-        KeySym keySym = to!KeySym(to!keyBaseType(tclArr[0].tclPeekString()));
-
-        auto input = tclArr[1].tclPeekString();
-        //~ stderr.writefln("input: %s", input);
-
-        char uniChar = input.empty ? char.init : to!char(input.front);
-
-        //~ stderr.writefln("unichar: %s", uniChar);
-
-        KeyMod keyMod = cast(KeyMod)to!long(tclArr[2].tclPeekString());
-
-        Widget widget = Widget.lookupWidgetPath(tclArr[3].tclPeekString());
+        Widget widget = getTclWidget(tclArr[3]);
         assert(widget !is null);
 
         Point widgetMousePos = getTclPoint(tclArr[4 .. 6]);
         Point desktopMousePos = getTclPoint(tclArr[6 .. 8]);
-
-        TimeMsec timeMsec = to!TimeMsec(tclArr[8].tclPeekString());
+        TimeMsec timeMsec = getTclTimestamp(tclArr[8]);
 
         auto event = scoped!KeyboardEvent(widget, keySym, uniChar, keyMod, widgetMousePos, desktopMousePos, timeMsec);
         return _dispatchEvent(widget, event);
@@ -220,8 +218,6 @@ static:
     /// create and populate a button event and dispatch it.
     private static TkEventFlag _handleButtonEvent(const Tcl_Obj*[] tclArr)
     {
-        //~ stderr.writefln("Key code: %s", cast(KeySym)to!long(tclArr[0].tclPeekString()));
-
         assert(tclArr.length == 2, tclArr.length.text);
 
         /**
@@ -230,12 +226,9 @@ static:
                 1  => Widget path
         */
 
-        //~ stderr.writefln("-- received #0: %s", tclArr[0].tclPeekString());
-        //~ stderr.writefln("-- received #1: %s", tclArr[1].tclPeekString());
-
         ButtonAction action = to!ButtonAction(tclArr[0].tclPeekString());
 
-        Widget widget = Widget.lookupWidgetPath(tclArr[1].tclPeekString());
+        Widget widget = getTclWidget(tclArr[1]);
         assert(widget !is null);
 
         // note: timestamp missing since -command doesn't have percent substitution
@@ -245,18 +238,9 @@ static:
         return _dispatchEvent(widget, event);
     }
 
-    /// create and populate a mouse event and dispatch it.
-    //~ private static TkEventFlag _handleMouseEvent(const Tcl_Obj*[] tclArr)
-    //~ {
-        //~ auto event = scoped!MouseEvent();
-        //~ return _dispatchEvent(event);
-    //~ }
-
     /// main dispatch function
     private static TkEventFlag _dispatchEvent(Widget widget, scope Event event)
     {
-        //~ stderr.writefln("Dispatching %s %s", widget, event);
-
         /** Handle the filter list, return if filtered. */
         _filterEvent(widget, event);
         if (event.handled)
@@ -296,7 +280,7 @@ static:
 
             case button:
                 StaticCast!Button(widget).onButtonEvent.call(StaticCast!ButtonEvent(event));
-                result = TkEventFlag.ok;  // -command events do not have continue/break
+                result = TkEventFlag.ok;  // -command events can only return TCL_OK
                 break;
 
             default: assert(0, format("Unhandled event type: '%s'", event.type));
@@ -305,7 +289,7 @@ static:
         /** Notify any listeners. */
         _notifyEvent(widget, event);
 
-        /** Handle event bubbling, this is the final travel stage. */
+        /** Handle event bubbling, this is the final dispatch stage. */
         _bubbleEvent(widget, event);
 
         /**
@@ -422,152 +406,9 @@ static:
             _bubbleEventImpl(parent, event);
     }
 
-    static extern(C)
-    int dtkCallbackHandler(ClientData clientData, Tcl_Interp* interp, int objc, const Tcl_Obj** argArr)
-    {
-        if (objc < 2)  // DTK event signals need at least 2 arguments
-            return TCL_OK;
-
-        // todo: store %t in the first run as _timeBegin, and then use %t - _timeBegin to calculate
-        // a Duration type (e.g. fromMsecs or similar).
-
-        // todo: store a Point() struct for the mouse point.
-        // todo:
-
-        /**
-            Indices:
-                0  => Name of this callback.
-                1  => The EventType.
-                2+ => The data, based on the Tcl substitutions that we provided for the EventType.
-        */
-
-        version (DTK_LOG_EVENTS)
-        {
-            import std.stdio;
-            foreach (idx; 0 .. objc)
-            {
-                stderr.writefln("-- received #%s: %s", idx + 1, argArr[idx].tclPeekString());
-            }
-        }
-
-        EventType type = to!EventType(argArr[1].tclPeekString());
-        auto args = argArr[2 .. objc];
-
-        switch (type) with (EventType)
-        {
-            case mouse:    return _handleMouseEvent(args);
-            case keyboard: return _handleKeyboardEvent(args);
-            case button:   return _handleButtonEvent(args);
-            default:       assert(0, format("Unhandled event type '%s'.", type));
-        }
-    }
-
-    // no-op
-    static extern(C)
-    void dtkCallbackDeleter(ClientData clientData) { }
-
-    /** Is the Tcl callback registered. */
+private:
+    /** Tcl callback registration state. */
     __gshared bool _dtkCallbackInitialized;
-}
-
-private void _oldValidation()
-{
-    //~ return TCL_OK;
-
-    // todo: use try/catch on a Throwable, we don't want to escape exceptions to the C side.
-    // todo: extract the types, and the event type, and the direct it to a D function that can
-
-   /+  int slotID = cast(int)clientData;
-
-    if (auto callback = slotID in _callbackMap)
-    {
-        Event event;  // todo: update
-
-        if (objc > 1)  // todo: objc is the argArr count, not sure if we should always assign all fields
-        {
-            try
-            {
-                event.type = tclConv!TkEventType(Tcl_GetString(argArr[1]));
-            }
-            catch (ConvException ce)
-            {
-                stderr.writefln("Couldn't convert: `%s`", to!string(Tcl_GetString(argArr[1])));
-                throw ce;
-            }
-
-            switch (event.type) with (TkEventType)
-            {
-                case TkCheckButtonToggle:
-                case TkRadioButtonSelect:
-                case TkComboboxChange:
-                case TkTextChange:
-                case TkListboxChange:
-                case TkProgressbarChange:
-                case TkScaleChange:
-                case TkSpinboxChange:
-                case TkCheckMenuItemToggle:
-                case TkRadioMenuSelect:
-                {
-                    event.state = to!string(Tcl_GetString(argArr[2]));
-                    break;
-                }
-
-                case TkValidate:
-                case TkFailedValidation:
-                {
-                    string validEventArgs = to!string(Tcl_GetString(argArr[2]));
-
-                    auto args = validEventArgs.splitter(" ");
-
-                    event.validateEvent.type = toValidationType(to!int(args.front));
-                    args.popFront();
-
-                    event.validateEvent.charIndex = to!sizediff_t(args.front);
-                    args.popFront();
-
-                    event.validateEvent.newValue = args.front == "{}" ? null : args.front;
-                    args.popFront();
-
-                    event.validateEvent.curValue = args.front == "{}" ? null : args.front;
-                    args.popFront();
-
-                    event.validateEvent.changeValue = args.front == "{}" ? null : args.front;
-                    args.popFront();
-
-                    event.validateEvent.validationMode = toValidationMode(args.front);
-                    args.popFront();
-
-                    event.validateEvent.validationCondition = toValidationMode(args.front);
-                    args.popFront();
-                    break;
-                }
-
-                default:
-                {
-                    foreach (idx, field; event.tupleof)
-                    static if (idx != 0)  // first element is the callback name
-                    {
-                        if (objc > idx)
-                        {
-                            //~ stderr.writefln("arg %s: %s", idx, to!string(Tcl_GetString(argArr[idx])));
-                            event.tupleof[idx - 1] = tclConv!(typeof(event.tupleof[idx - 1]))(Tcl_GetString(argArr[idx]));
-                        }
-                    }
-                }
-            }
-        }
-
-        //~ stderr.writefln("emitting: %s %s", callback.widget, event);
-        callback.signal.emit(callback.widget, event);
-        return TCL_OK;
-    }
-    else
-    {
-        Tcl_SetResult(interp, cast(char*)"Trying to invoke non-existent callback", TCL_STATIC);
-        return TCL_ERROR;
-    } +/
-
-    //~ return TCL_OK;
 }
 
 /** Extract the integral X and Y points from the 2-dimensional Tcl_Obj array. */
@@ -575,6 +416,12 @@ private Point getTclPoint(ref const(Tcl_Obj*)[2] tclArr)
 {
     return Point(to!int(tclArr[0].tclPeekString()),
                  to!int(tclArr[1].tclPeekString()));
+}
+
+/** Extract the mouse action from the Tcl_Obj object. */
+private MouseAction getTclMouseAction(const(Tcl_Obj)* tclObj)
+{
+    return to!MouseAction(tclObj.tclPeekString());
 }
 
 /** Extract the mouse button from the Tcl_Obj object. */
@@ -613,4 +460,29 @@ private int getTclWheelDelta(const(Tcl_Obj)* tclObj)
     return (wheelStr == "??") ? 0 : to!int(wheelStr);
 }
 
+/** Extract the key symbol from the Tcl_Obj object. */
+private KeySym getTclKeySym(const(Tcl_Obj)* tclObj)
+{
+    // note: change this to EnumBaseType after Issue 10942 is fixed for KeySym.
+    alias keyBaseType = long;
+    return to!KeySym(to!keyBaseType(tclObj.tclPeekString()));
+}
 
+/** Extract the unicode character from the Tcl_Obj object. */
+private char getTclUniChar(const(Tcl_Obj)* tclObj)
+{
+    auto input = tclObj.tclPeekString();
+    return input.empty ? char.init : to!char(input.front);
+}
+
+/** Extract the Widget from the Tcl_Obj object. Return null if not found. */
+private Widget getTclWidget(const(Tcl_Obj)* tclObj)
+{
+    return Widget.lookupWidgetPath(tclObj.tclPeekString());
+}
+
+/** Extract the timestamp from the Tcl_Obj object. */
+private TimeMsec getTclTimestamp(const(Tcl_Obj)* tclObj)
+{
+    return to!TimeMsec(tclObj.tclPeekString());
+}
