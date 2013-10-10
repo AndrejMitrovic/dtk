3af167d6f22bc2249614fb48d555ff06ca4e7764
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-10-10 23:11:20 +0200

    test styles

--- tests/test_cursor.d
+++ tests/test_cursor.d
@@ -1,4 +1,4 @@
-module test_cursors;
+module test_cursor;
 
 import core.thread;
 
---
+++ tests/test_style.d
@@ -0,0 +1,24 @@
+module test_style;
+
+import std.conv;
+import std.range;
+import std.stdio;
+import std.string;
+
+import dtk;
+
+unittest
+{
+    auto app = new App;
+
+    auto testWindow = new Window(app.mainWindow, 200, 200);
+    testWindow.position = Point(500, 500);
+
+    auto button = new Button(testWindow);
+
+    app.run();
+}
+
+void main()
+{
+}
