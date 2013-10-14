c800bb99a0445c9795837152edee798e61630c08
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2013-10-14 20:33:20 +0200

    Remove std.exception import.

--- src/dtk/imports/inline.d
+++ src/dtk/imports/inline.d
@@ -25,9 +25,11 @@ public import core.time
 public import core.thread
     : Thread;
 
+// ok
 public import core.stdc.config
     : c_long, c_ulong;
 
+// ok
 public import core.stdc.string
     : memcpy;
 
@@ -40,9 +42,6 @@ public import std.algorithm
 public import std.array
     : Appender, array, replace, empty, split;
 
-private import std.range
-    : chain;
-
 immutable digits         = "0123456789";                 /// 0..9
 immutable letters        = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ~
                            "abcdefghijklmnopqrstuvwxyz"; /// A..Za..z
@@ -54,15 +53,6 @@ immutable whitespace     = " \t\v\r\n\f";                /// ASCII whitespace
 bool isDigit(dchar c) @safe pure nothrow
 {
     return c <= 0x7F ? cast(bool)(_ctype[c] & _DIG) : false;
-}
-
-unittest
-{
-    foreach(c; digits)
-        assert(isDigit(c));
-
-    foreach(c; chain(letters, whitespace))
-        assert(!isDigit(c));
 }
 
 immutable ubyte[128] _ctype =
@@ -108,8 +98,96 @@ public import std.conv
 public import std.datetime
     : StopWatch, AutoStart, seconds, msecs;
 
-public import std.exception
-    : assertThrown, enforce;
+/++
+    Asserts that the given expression throws the given type of $(D Throwable).
+    The $(D Throwable) is caught and does not escape assertThrown. However,
+    any other $(D Throwable)s $(I will) escape, and if no $(D Throwable)
+    of the given type is thrown, then an $(D AssertError) is thrown.
+
+    Params:
+        T          = The $(D Throwable) to test for.
+        expression = The expression to test.
+        msg        = Optional message to output on test failure.
+        file       = The file where the error occurred.
+                     Defaults to $(D __FILE__).
+        line       = The line where the error occurred.
+                     Defaults to $(D __LINE__).
+
+    Throws:
+        $(D AssertError) if the given $(D Throwable) is not thrown.
+  +/
+void assertThrown(T : Throwable = Exception, E)
+                 (lazy E expression,
+                  string msg = null,
+                  string file = __FILE__,
+                  size_t line = __LINE__)
+{
+    try
+        expression();
+    catch (T)
+        return;
+
+    throw new AssertError(phobosFormat("assertThrown failed: No %s was thrown%s%s",
+                                 T.stringof, msg.empty ? "." : ": ", msg),
+                          file, line);
+}
+
+/++
+    If $(D !!value) is true, $(D value) is returned. Otherwise,
+    $(D new Exception(msg)) is thrown.
+
+    Note:
+        $(D enforce) is used to throw exceptions and is therefore intended to
+        aid in error handling. It is $(I not) intended for verifying the logic
+        of your program. That is what $(D assert) is for. Also, do not use
+        $(D enforce) inside of contracts (i.e. inside of $(D in) and $(D out)
+        blocks and $(D invariant)s), because they will be compiled out when
+        compiling with $(I -release). Use $(D assert) in contracts.
+
+    Example:
+    --------------------
+    auto f = enforce(fopen("data.txt"));
+    auto line = readln(f);
+    enforce(line.length, "Expected a non-empty line.");
+    --------------------
+ +/
+T enforce(T)(T value, lazy const(char)[] msg = null, string file = __FILE__, size_t line = __LINE__)
+{
+    if (!value) bailOut(file, line, msg);
+    return value;
+}
+
+/++
+   $(RED Scheduled for deprecation in January 2013. If passing the file or line
+         number explicitly, please use the version of enforce which takes them as
+         function arguments. Taking them as template arguments causes
+         unnecessary template bloat.)
+ +/
+T enforce(T, string file, size_t line = __LINE__)
+    (T value, lazy const(char)[] msg = null)
+{
+    if (!value) bailOut(file, line, msg);
+    return value;
+}
+
+/++
+    If $(D !!value) is true, $(D value) is returned. Otherwise, the given
+    delegate is called.
+
+    The whole safety and purity are inferred from $(D Dg)'s safety and purity.
+ +/
+T enforce(T, Dg, string file = __FILE__, size_t line = __LINE__)
+    (T value, scope Dg dg)
+    if (isSomeFunction!Dg && is(typeof( dg() )))
+{
+    if (!value) dg();
+    return value;
+}
+
+private void bailOut(string file, size_t line, in char[] msg) @safe pure
+{
+    throw new Exception(msg ? msg.idup : "Enforcement failed", file, line);
+}
 
 public import std.file
     : exists, isFile;
