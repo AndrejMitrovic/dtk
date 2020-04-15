365fd8e7d40a0e68e29857f476b40ae2be7a190d
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-04-15 16:12:50 +0900

    Update copyright years.

--- src/dtk/loader.d
+++ src/dtk/loader.d
@@ -1,5 +1,5 @@
 /*
- *             Copyright Andrej Mitrovic 2013.
+ *         Copyright Andrej Mitrovic 2013 - 2020.
  *  Distributed under the Boost Software License, Version 1.0.
  *     (See accompanying file LICENSE_1_0.txt or copy at
  *           http://www.boost.org/LICENSE_1_0.txt)
