db28b635715c9d96b0c09d86a93dec977a5e6c8e
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2020-04-19 21:07:39 +0900

    Remove duplicate test runner
    Fixes #51

--- src/dtk/tests/globals.d
+++ src/dtk/tests/globals.d
@@ -14,8 +14,9 @@ version(unittest):
 /** One app and one main window for testing. */
 import dtk.app;
 import dtk.geometry;
-import dtk.tests.runner;
 import dtk.widgets.window;
+
+import core.runtime;
 
 __gshared App app;
 
@@ -61,5 +62,96 @@ shared static this()
     // http://d.puremagic.com/issues/show_bug.cgi?id=10851
     assert(app.mainWindow !is null);
     app.mainWindow.position = Point(500, 500);
-    unitTester.setTester();
+    Runtime.extendedModuleUnitTester = &customModuleUnitTester;
+}
+
+/// Enables filtering tests via the 'dtest' environment variable
+private UnitTestResult customModuleUnitTester ()
+{
+    import std.algorithm;
+    import std.parallelism;
+    import std.process;
+    import std.range;
+    import std.stdio;
+    import std.string;
+    import std.uni;
+    import core.atomic;
+    import core.sync.mutex;
+
+    string[] skip_mods = environment.get("dskip").toLower().split(",");
+    string filter = environment.get("dtest").toLower();
+    size_t filtered;
+
+    struct ModTest
+    {
+        string name;
+        void function() test;
+    }
+
+    ModTest[] mod_tests;
+    LForeach: foreach (ModuleInfo* mod; ModuleInfo)
+    {
+        if (mod is null)
+            continue;
+
+        auto fp = mod.unitTest;
+        if (fp is null)
+            continue;
+
+        // only run this repo's tests, not any dependencies
+        if (mod.name.startsWith("dtk"))
+        {
+            if (filter.length > 0 &&
+                !canFind(mod.name.toLower(), filter))
+            {
+                filtered++;
+                continue;
+            }
+
+            foreach (skip_mod; skip_mods)
+            {
+                if (canFind(mod.name.toLower(), skip_mod))
+                {
+                    filtered++;
+                    continue LForeach;
+                }
+            }
+
+            mod_tests ~= ModTest(mod.name, fp);
+        }
+    }
+
+    shared size_t executed;
+    shared size_t passed;
+    shared Mutex print_lock = new shared Mutex();
+
+    void runTest (ModTest mod)
+    {
+        atomicOp!"+="(executed, 1);
+        try
+        {
+            //writefln("Unittesting %s..", mod.name);
+            mod.test();
+            atomicOp!"+="(passed, 1);
+        }
+        catch (Throwable ex)
+        {
+            synchronized (print_lock)
+            {
+                writefln("Module tests failed: %s", mod.name);
+                writeln(ex);
+            }
+        }
+    }
+
+    foreach (mod; mod_tests)
+        runTest(mod);
+
+    UnitTestResult result = { executed : executed, passed : passed };
+    if (filtered > 0)
+        writefln("Ran %s/%s tests (%s filtered)", result.executed,
+            result.executed + filtered, filtered);
+
+    result.runMain = false;
+    return result;
 }
